0627ec2a0a4ed44982c1ce031e79dfcd493e47e4
Alexander Carrillo <alexandergcca@gmail.com>
2021-02-08 21:11:03 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Práctica de Python - Inteligencia Artificial.
+# Alexander Carrillo
+
+nombre = "Alexander"
+
+apellido = "Carrillo"
+
+print("Ingresa primero tu nombre y luego tu apellido.")
+
+entradaName = input()
+entradaApellido = input()
+
+if entradaName == nombre and entradaApellido == apellido:
+    print("################################## \n")
+    print(f"Bienvenido {nombre} {apellido} !!! \n")
+    print("##################################")
+else: 
+    print("Intruso, no eres Alexander.")
